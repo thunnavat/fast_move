61c83b0a96365ef1248d44291826c1888c671474
champ555777@gmail.com <champ555777@gmail.com>
2020-11-10 17:28:38 +0700

    Add 10 Parcles

--- src/data/ToyData.java
+++ src/data/ToyData.java
@@ -1,7 +1,9 @@
 package data;
 
 import base.Bike;
+import base.Location;
 import base.Parcel;
+import base.ParcelStatus;
 import base.Person;
 
 public class ToyData {
@@ -22,7 +24,16 @@ public class ToyData {
 
     public static Parcel[] getParcels() {
         Parcel[] parcels = new Parcel[10];
-        // your code
+        parcels[0] = new Parcel(5.0,ParcelStatus.READY_TO_SHIP,new Location(25,10.0));
+        parcels[1] = new Parcel(2.0,ParcelStatus.SHIPPING,new Location(20.0,15.0));
+        parcels[2] = new Parcel(10.0,ParcelStatus.DELIVERED,new Location(27.0,14.0));
+        parcels[3] = new Parcel(55.0,ParcelStatus.READY_TO_SHIP,new Location(19.0,14.0));
+        parcels[4] = new Parcel(14.0,ParcelStatus.READY_TO_SHIP,new Location(14.0,19.0));
+        parcels[5] = new Parcel(9.0,ParcelStatus.DELIVERED,new Location(55.0,12.0));
+        parcels[6] = new Parcel(10.0,ParcelStatus.SHIPPING,new Location(2.0,20.0));
+        parcels[7] = new Parcel(5.0,ParcelStatus.READY_TO_SHIP,new Location(10.0,15.0));
+        parcels[8] = new Parcel(40.0,ParcelStatus.READY_TO_SHIP,new Location(6.0,1.0));
+        parcels[9] = new Parcel(21.0,ParcelStatus.READY_TO_SHIP,new Location(12.0,19.0));
 
         return parcels;
     }
